4fbbafbd4235f0dfb2bb4c5fb780297a08b49290
Milan Kovačević <93384395+Milan-Kovacevic@users.noreply.github.com>
2025-05-10 13:33:45 +0200

    feat(core): trigger semantic release BREAKING CHANGE: Refactor type definitions and remove unused extensions. Update FileSystemAccess API for improved file handling. Generalize indexed db store provider types.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-import useFileSystemAccess from "./core/use-fs-access";
+import useFileSystemAccess, {
+  isApiSupported,
+  showDirectoryPicker,
+} from "./core/use-fs-access";
 export default useFileSystemAccess;
+export { isApiSupported, showDirectoryPicker };
